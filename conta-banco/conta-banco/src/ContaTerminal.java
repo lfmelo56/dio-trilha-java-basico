9ef08022a853433959e26c58f974be2cd43059d4
Luís Felipe Melo <luisfelipemelo56@gmail.com>
2024-09-04 20:54:41 -0300

    Conta bancária criada com as perguntas e configurada pra nome e sobrenome estarem em uma única linha

--- conta-banco/conta-banco/src/ContaTerminal.java
+++ conta-banco/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome e sobrenome");
+        String nome = scanner.nextLine();  // NextLine() pra evitar que envie a pergunta da agência e da conta de vez
+
+        System.out.println("Digite o número da sua agência");
+        String agencia = scanner.nextLine();  
+
+        System.out.println("Digite o número da sua conta");
+        int numero = scanner.nextInt();
+        scanner.nextLine();  
+
+        System.out.println("Digite o seu saldo");
+        double saldo = scanner.nextDouble();
+        scanner.nextLine();  
+
+        // Imprimindo dados obtidos pelo usuário
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
